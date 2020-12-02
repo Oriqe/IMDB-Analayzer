bb30e47b1d847a5676fc8572797858ec1058b1cc
Oriqe <60728090+Oriqe@users.noreply.github.com>
2020-12-02 22:54:57 +0200

    addind server functionality

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ var path = require("path");
 const fs = require('fs')
 const write_stream = fs.createWriteStream('resources/data.js')
 var routes = require("./routes")
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'resources')))
+app.set("views", path.join(__dirname, "views"));
+app.use(routes)
+
+fetch = require("node-fetch");
+
+async function scraping() {
+
+}
+
+
+
+scraping.then(data => {
+    app.listen(app.get("port"), function(){
+      console.log('Finished scraping and writing, The server is available at http://127.0.0.1:3000/')
+    })
+  })
