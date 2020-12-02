0ef73e86981ea44181ef7d54bb79ca06fb15cfba
Oriqe <60728090+Oriqe@users.noreply.github.com>
2020-12-02 23:04:42 +0200

    adding scraping code for the initial spreadsheet data page

--- app.js
+++ app.js
@@ -12,10 +12,33 @@ app.use(routes)
 
 fetch = require("node-fetch");
 
+
 async function scraping() {
-
-}
-
+    console.log("Please wait for a couple of seconds until the data-scraping is completed")
+    
+      const address = await fetch("https://docs.google.com/spreadsheets/d/1--FMdHdg_fnfBvebZwngB1pukZCrlPO5frcVazuP58Q/edit", {
+        "headers": {
+          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "accept-language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
+          "cache-control": "max-age=0",
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "same-origin",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": "1",
+          "x-client-data": "CIm2yQEIorbJAQjBtskBCKmdygEIl6zKAQjquMoBCK3BygEIrMfKAQj2x8oBCLTLygEI3NXKAQjwl8sBCJeaywEYisHKAQ=="
+        },
+        "referrer": "https://docs.google.com/spreadsheets/d/1--FMdHdg_fnfBvebZwngB1pukZCrlPO5frcVazuP58Q/edit",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors",
+        "credentials": "include"
+      })
+      .then((response) => response.text())
+  
+    }
+  
 
 
 scraping.then(data => {
